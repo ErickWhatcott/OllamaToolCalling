1f8db54ec5d6568b4a936542cfac7451d70052e8
Erick Whatcott <whatcotterick@gmail.com>
2026-03-02 20:22:04 -0700

    Modified the structure of SelectAsync for generating bills such that if you invoke  Console.CancelKeyPress it will terminate the generation of the bills, but still create the csv.

--- GenerateSampleData/Extensions/IAsyncEnumerableExtensions.cs
+++ GenerateSampleData/Extensions/IAsyncEnumerableExtensions.cs
@@ -1,10 +1,20 @@
+// A compiler warning raised by the compiler due to the strange pattern of select async.
+#pragma warning disable CS8425
+
 public static class IAsyncEnumerableExtensions
 {
-    public static async IAsyncEnumerable<V> SelectAsync<T, V>(this IAsyncEnumerable<T> values, Func<T, Task<V>> func)
+    // Specialized function for asynchronous selection and early termination (as compared to the normal exception)
+    // With a cancelation token.
+    public static async IAsyncEnumerable<V> SelectAsync<T, V>(this IAsyncEnumerable<T> values, Func<T, Task<V>> func, CancellationToken token)
     {
         await foreach(var v in values)
         {
+            if(token.IsCancellationRequested)
+                yield break;
+
             yield return await func(v);
         }
     }
 }
+
+#pragma warning restore CS8425
--- GenerateSampleData/Program.cs
+++ GenerateSampleData/Program.cs
@@ -4,7 +4,14 @@ using CsvHelper;
 const bool DEBUG = false;
 const int LAWYERS = 8;
 const int CLIENTS = 25;
-const int BILLS = 100;
+const int BILLS = 50_000;
+
+CancellationTokenSource cts = new();
+Console.CancelKeyPress += (s, e) =>
+{
+    cts.Cancel();
+    e.Cancel = true;
+};
 
 string outputPath = Path.Combine(
     Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
@@ -19,12 +26,9 @@ var bills = await AsyncEnumerable.Range(0, count: BILLS).SelectAsync(async i =>
 {
     Console.WriteLine($"Making bill {i}");
     return await func.GenerateBillable(lawyers, clients, DateTime.Today.AddDays(-7), DateTime.Today);
-}).ToListAsync();
+}, cts.Token).ToListAsync();
 
 using var writer = new StreamWriter(outputPath, false);
 using var csv = new CsvWriter(writer, CultureInfo.CurrentCulture);
 
-// csv.WriteHeader<OllamaFunc.BillRecord>();
 csv.WriteRecords(bills);
-// foreach (var bill in bills)
-//     csv.WriteRecord(bill);
